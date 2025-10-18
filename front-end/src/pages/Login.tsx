8d6ba253130e13ac4ee68d9159c5a7faaa6332b2
lukinhasc-dev <lucas.sca2022@gmail.com>
2025-10-18 15:05:27 -0300

    ajustes na pagina de login pt2

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -5,29 +5,29 @@ export default function Login() {
     <div className='bg-blue-950 flex flex-col justify-center items-center h-screen w-screen'>
 
       {/*Card*/}
-      <div className='bg-white rounded-lg shadow-lg p-8 flex flex-col justify-center items-center gap-2'>
+      <div className='bg-white rounded-lg shadow-lg p-8 flex flex-col justify-center items-center w-auto h-auto'>
         <img src={Assets} alt="SpeedAuto" className='flex justify-center items-center w-10 h-15' />
-        <h1 className='font-bold font-poppins text-2xl'>Bem-vindo de volta!</h1>
+        <h1 className='font-bold font-poppins text-2xl'>Seja bem-vindo!</h1>
         <p className='font-poppins text-1xl'>Gerencie seus veículos, vendas e clientes.</p>
 
 
         {/*Forms*/}
         <form className='flex flex-col gap-0 mt-3'>
-          <label htmlFor="email" className=''>Email</label>
+          <label htmlFor="email" className='mb-2 mt-3'>Email</label>
           <input
             type="email"
             id="email"
             placeholder="Insira seu Email"
-            className='border border-gray-300 rounded-lg p-2'
+            className='border bg-[#EDF2F7] border-gray-200 rounded-lg p-2 font-poppins mb-2'
           />
-          <label htmlFor="passsword">Senha</label>
+          <label htmlFor="passsword" className='mb-2 mt-3'>Senha</label>
           <input
             type="password"
             id="password"
             placeholder="Insira sua Senha"
-            className='border border-gray-300 rounded-lg p-2'
+            className='border bg-[#EDF2F7] border-gray-200 rounded-lg p-2 font-poppins mb-2'
           />
-          <button className='bg-blue-950 text-white rounded-lg p-2'>Entrar</button>
+          <button className='bg-[#1f52dc] text-white rounded-lg p-2 font-semibold mt-10'>Entrar</button>
         </form>
       </div>
 

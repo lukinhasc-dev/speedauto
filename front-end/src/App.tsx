2d06681b41ceebc48104a04a51f82d316edc99fd
Vitor Alexandre <v.alexandre.dsilva@gmail.com>
2025-10-18 18:38:51 -0300

    Novas Adições de Páginas

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+
+interface PlaceholderProps {
+  title: string;
+}
+
+const Placeholder: React.FC<PlaceholderProps> = ({ title }) => {
+  return <h1 className="text-3xl font-bold">{title}</h1>;
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/pagina-inicial" element={<Sidebar />} />
+        <Route path="/pagina-inicial" element={<Dashboard/>} />
+        <Route path="veiculos" element={<Placeholder title="Página de Veículos" />} />
+        <Route path="vendas" element={<Placeholder title="Página de Vendas" />} />
+        <Route path="clientes" element={<Placeholder title="Página de Clientes" />} />
+        <Route path="relatorios" element={<Placeholder title="Página de Relatórios" />} />
+        <Route path="configuracoes" element={<Placeholder title="Página de Configurações" />} />
       </Routes>
     </BrowserRouter>
   );

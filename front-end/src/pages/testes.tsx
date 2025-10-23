75599574cdd9131627bbf3e1e896ccba314a53a7
Vitor Alexandre <v.alexandre.dsilva@gmail.com>
2025-10-23 00:20:55 -0300

    teste

---
+++ front-end/src/pages/testes.tsx
@@ -0,0 +1,3 @@
+import React, { useState, useMemo } from 'react';
+import { FaDownload, FaChartLine, FaCarSide, FaUsers, FaDollarSign, FaCalendarAlt, FaCar, FaUserTie, FaArrowUp, FaArrowDown, FaTimes, FaReceipt } from 'react-icons/fa'; // Importa FaTimes
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, PieChart, Pie, Cell, Legend } from 'recharts';

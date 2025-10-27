6d7e6bbb670e65966781fc5c741b36926ad674a5
FhVieira13 <flaviohvieira20@gmail.com>
2025-10-27 01:58:49 -0300

    Adição do arquivo index.html, previas de uma possivel janela de chatbot

--- front-end/src/pages/testes.tsx
+++
@@ -1,2 +0,0 @@
-import React, { useState, useMemo } from 'react';
-import { FaDownload, FaChartLine, FaCarSide, FaUsers, FaDollarSign, FaCalendarAlt, FaCar, FaUserTie, FaArrowUp, FaArrowDown, FaTimes, FaReceipt } from 'react-icons/fa'; // Importa FaTimes

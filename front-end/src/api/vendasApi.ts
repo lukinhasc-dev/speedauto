33b0624c11de9b7f09851844c2a126d1580d80b9
lukinhasc-dev <lucas.sca2022@gmail.com>
2025-11-16 20:00:45 -0300

    Página de Relatórios 90% finalizada.

--- front-end/src/api/vendasApi.ts
+++ front-end/src/api/vendasApi.ts
@@ -1,25 +1,25 @@
 import axios from 'axios';
-import type { Vendas } from '../types/Venda';
+import type { Venda } from '../types/Venda';
 
 const API_URL = 'http://localhost:5000/vendas';
 
-export const getVendas = async (): Promise<Vendas[]> => {
-    const response = await axios.get<Vendas[]>(API_URL);
+export const getVendas = async (): Promise<Venda[]> => {
+    const response = await axios.get<Venda[]>(API_URL);
     return response.data;
 }
 
-export const getVendaById = async (id: number): Promise<Vendas> => {
-    const response = await axios.get<Vendas>(`${API_URL}/${id}`);
+export const getVendaById = async (id: number): Promise<Venda> => {
+    const response = await axios.get<Venda>(`${API_URL}/${id}`);
     return response.data;
 }
 
-export const addVenda = async (venda: Omit<Vendas, 'id'>): Promise<Vendas> => {
-    const response = await axios.post<Vendas>(API_URL, venda);
+export const addVenda = async (venda: Omit<Venda, 'id'>): Promise<Venda> => {
+    const response = await axios.post<Venda>(API_URL, venda);
     return response.data;
 }
 
-export const updateVenda = async (id: number, venda: Partial<Omit<Vendas, 'id'>>): Promise<Vendas> => {
-    const response = await axios.put<Vendas>(`${API_URL}/${id}`, venda);
+export const updateVenda = async (id: number, venda: Partial<Omit<Venda, 'id'>>): Promise<Venda> => {
+    const response = await axios.put<Venda>(`${API_URL}/${id}`, venda);
     return response.data;
 }
 
